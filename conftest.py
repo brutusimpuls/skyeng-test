39c949fc9837e2ef6c0a3bd7039ff587c09f5dca
igor <brutusimpuls@gmail.com>
2021-05-19 22:50:32 +0300

    epic games tests

---
+++ conftest.py
@@ -0,0 +1,15 @@
+#-*- coding: utf-8 -*-
+from selenium import webdriver
+import pytest
+from config import *
+
+
+@pytest.fixture(scope="session")
+def browser(request):
+    browser = webdriver.Firefox()
+    browser.maximize_window()
+    browser.implicitly_wait(45)
+    yield browser
+    browser.quit()
+    #request.addfinalizer(driver.quit)
+    #return driver
